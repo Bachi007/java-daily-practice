bf7ad74f74896bf3761abe343adf58bffaf80f53
Bhaskara Sai Chitturi <34607489+Bachi007@users.noreply.github.com>
2023-02-23 20:27:20 +0530

    practice on 23-02

---
+++ meovlo2.java
@@ -0,0 +1,19 @@
+class meovlo2
+{
+
+	public static void main(String args[])
+	{
+
+		addition(51,42,62,19,20,15,1200,101);
+
+	}
+	public static void addition(int ...a)
+	{
+		int sum=0;
+		for(int i:a)
+		{
+			sum+=i;
+		}
+		System.out.println(sum);
+	} 
+}
