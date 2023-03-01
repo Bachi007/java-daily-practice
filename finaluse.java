6056a04bdd7c55768d59a10605e25e9a3defc857
Bhaskara Sai Chitturi <34607489+Bachi007@users.noreply.github.com>
2023-03-01 20:26:25 +0530

    practice on 01-03

---
+++ finaluse.java
@@ -0,0 +1,39 @@
+class finaluse
+{
+
+	public static void main(String args[])
+	{
+
+		final int num=10;
+		System.out.println(num);
+		//num=15;
+		System.out.println(num);
+			
+			SBI s=new SBI();
+			s.fixeddeposit();	
+	}
+
+}
+
+class RBI
+{
+
+	public final void fixeddeposit()
+	{
+		System.out.println("Fixed deposit in RBI");
+	}
+
+}
+
+
+class SBI extends RBI
+{
+	public void fixeddeposit()
+	{
+		System.out.println("Fixed deposit in SBI");
+	}
+}
+
+
+
+
---
+++ finaluse2.java
@@ -0,0 +1,33 @@
+class finaluse2
+{
+
+	public static void main(String args[])
+	{
+			
+			SBI s=new SBI();
+			s.fixeddeposit();	
+	}
+
+}
+
+
+final class RBI
+{
+
+	public void fixeddeposit()
+	{
+		System.out.println("Fixed deposit in RBI");
+	}
+
+}
+
+
+class SBI extends RBI
+{
+	public void fixeddeposit()
+	{
+		System.out.println("Fixed deposit in SBI");
+	}
+}
+
+
