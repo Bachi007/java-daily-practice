e8ab98f398b416188745e07c8cc7621561edad06
Bhaskara Sai Chitturi <34607489+Bachi007@users.noreply.github.com>
2023-03-02 20:37:39 +0530

    practice on 02-03

---
+++ rrr.java
@@ -0,0 +1,11 @@
+package bhaskara;
+
+public class rrr
+{
+
+	public void natunatu()
+	{
+		System.out.println("Natu Natu nominated for Oscars 2023");
+	}
+
+}
---
+++ tollywood.java
@@ -0,0 +1,13 @@
+package movie;
+import bhaskara.*;
+
+public class tollywood
+{
+	public static void main(String args[])
+	{
+			rrr r1=new rrr();
+			r1.natunatu();
+	}
+
+
+}
