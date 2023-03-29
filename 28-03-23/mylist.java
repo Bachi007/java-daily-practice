00849affc9d6d0b69f62e049d57d358714798119
Bhaskara Sai Chitturi <34607489+Bachi007@users.noreply.github.com>
2023-03-29 18:55:13 +0530

    Update mylist.java

--- 28-03-23/mylist.java
+++ 28-03-23/mylist.java
@@ -4,7 +4,7 @@ class mylist{
 
 	public static void main(String args[])
 	{
-		List<String> ln=Arrays.asList(5,6,7,9,12,8,10);
+		List<Integer> ln=Arrays.asList(5,6,7,9,12,8,10);
 		
 		//for(int a:ln)
 		//{
